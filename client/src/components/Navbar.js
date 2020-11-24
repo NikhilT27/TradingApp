3b9e157f684edbf436de2bf9675b0ccf9a370cd6
NikhilT27 <nikhilthakare14@gmail.com>
2020-11-24 13:51:41 +0530

    add: home link to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar({ children }) {
   return (
     <>
-      <div className="title-box">
-        <div>
-          <p>XXXX</p>
+      <Link to="/">
+        <div className="title-box">
+          <div>
+            <p>XXXX</p>
+          </div>
         </div>
-      </div>
+      </Link>
       {children}
       {/* <Footer /> */}
     </>
